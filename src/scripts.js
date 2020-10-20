06f2cb1eb4d83278a2aee6b7a5f216361d20e563
ThiagoCantarim <54301380+ThiagoCantarim@users.noreply.github.com>
2020-10-20 01:53:48 -0300

    Update scripts.js
    Adicionada algumas verificações adicionais, e botão reset...

--- src/scripts.js
+++ src/scripts.js
@@ -21,10 +21,23 @@ function vrfNumber(n){
     }
 }
 
-//Limpa o campo
-function cleanField(field){
-    field.value = ''
+//Limpa os campos
+function cleanField(id, num_field, slct_field, res_field){
+    if(id == 0){
+        num_field.value = '';
+    } else {
+        num_field.value = '';
+
+        while(slct_field.firstChild){
+            slct_field.removeChild(slct_field.firstChild);
+        }
+
+        while(res_field.firstChild){
+            res_field.removeChild(res_field.firstChild);
+        }
+    }
 }
+
 
 //Adiciona o número para a análise
 function addNumber(){ 
@@ -37,31 +50,75 @@ function addNumber(){
         item.text = `O número ${num.value} foi adicionado para análise!`;
         list.appendChild(item);
 
-        cleanField(num);
+        cleanField(0, num);
     } else {
         alert(error);
     }
     num.focus()
 }
 
+//Processa os dados do array
 function processNumbers(){
-    let res = document.querySelector('div#result');
-    let tamArray = aNumber.length;
-    let ordArray = aNumber.sort((a, b) => a - b);
-    let somaArray = 0;
+    if(aNumber.length < 2){
+        return alert('São nescessários ao menos 2 números para a análise!')
+    } else {
+        let lenArray = aNumber.length;
+        let sortArray = aNumber.sort((a, b) => a - b);
+        let sumArray = 0;
 
-    for(let i in ordArray){
-        somaArray += ordArray[i];
+        for(let i in sortArray){
+            sumArray += sortArray[i];
+        }
+
+        let avrgArray = sumArray / lenArray;
+        
+        showResult(lenArray, sortArray, sumArray, avrgArray);
     }
-
-    let medArray = somaArray / tamArray;
-   
-    res.innerHTML = ` ••••••••••••• Resultado da análise: •••••••••••••<br/>`
-    res.innerHTML += `• Ao todo foram analisados ${tamArray} números.<br/>`
-    res.innerHTML += `• O menor número informado foi ${ordArray[0]}.<br/>`
-    res.innerHTML += `• O maior número informado foi ${ordArray[tamArray-1]}.<br/>`
-    res.innerHTML += `• A soma de todos os números é ${somaArray}.<br/>`
-    res.innerHTML += `• A média dos números analisados é ${medArray.toFixed(2)}.<br/>`
 }
+
+//Apresenta o resultado
+function showResult(len, ord, sum, avrg){
+    let res = document.querySelector('div#result');
+
+    res.innerHTML = ` ••••••••••••• Resultado da análise: •••••••••••••<br/>`
+    res.innerHTML += `• Ao todo foram analisados ${len} números.<br/>`
+    res.innerHTML += `• O menor número informado foi ${ord[0]}.<br/>`
+    res.innerHTML += `• O maior número informado foi ${ord[len-1]}.<br/>`
+    res.innerHTML += `• A soma de todos os números é ${sum}.<br/>`
+    res.innerHTML += `• A média dos números analisados é ${avrg.toFixed(2)}.<br/><br/>`
+
+    resetButton(res);
+}
+
+//Adiciona botao reset
+function resetButton(res){
+    res.innerHTML += '<input type="button" id="btnReset" value="Reiniciar..." onclick="resetData()">'
+    let btnReset = document.getElementById('btnReset');
+
+    btnReset.style.textAlign = "center";
+    btnReset.style.borderStyle = "none"
+    btnReset.style.padding = "10px";
+    btnReset.style.width = "150px"
+    btnReset.style.borderRadius = "20px"
+    btnReset.style.boxShadow = "3px 3px 5px rgba(0, 0, 0, .4)";
+
+}
+
+//Reseta todos os dados
+function resetData() {
+    let num  = document.querySelector('input#nNumber');
+    let list = document.querySelector('select#msg-box');
+    let res  = document.querySelector('div#result');
+
+    while(aNumber.length){
+        aNumber.shift();
+    }
+    
+    cleanField(1, num, list, res);
+
+    num.focus();
+
+}
+
 
 
